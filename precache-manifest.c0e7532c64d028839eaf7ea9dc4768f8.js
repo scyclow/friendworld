e70833cf55d15a434abe5d641a652660d083861f
scyclow <spike716@gmail.com>
2019-03-10 22:10:27 -0400

    Updates

---
+++ precache-manifest.c0e7532c64d028839eaf7ea9dc4768f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59900b99f7e6a5cedda3",
+    "url": "/friendworld/static/js/runtime~main.59900b99.js"
+  },
+  {
+    "revision": "ad930f4c5fbd5c1158f4",
+    "url": "/friendworld/static/js/main.ad930f4c.chunk.js"
+  },
+  {
+    "revision": "29cee8b6f1c88622296a",
+    "url": "/friendworld/static/js/2.29cee8b6.chunk.js"
+  },
+  {
+    "revision": "ad930f4c5fbd5c1158f4",
+    "url": "/friendworld/static/css/main.f604e173.chunk.css"
+  },
+  {
+    "revision": "888c70d9b53610a6cd1c9c672599996e",
+    "url": "/friendworld/index.html"
+  }
+];
